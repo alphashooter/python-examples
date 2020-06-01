35b347a596c662c80971340cf7c8ea0fb54d7980
Andrey Strelkov <a.p.strelkov@gmail.com>
2020-06-01 10:38:14 +0300

    tkinter

--- misc/window_example.py
+++ misc/window_example.py
@@ -2,7 +2,7 @@ from time import time, sleep
 from math import sin, cos, pi
 from misc.window import Window
 
-window = Window(100, 100, 800, 600)
+window = Window(100, 100, 800, 600)  # create window 800x600 at (100, 100)
 fps = 60  # frames per second/updates per second
 radius = 50
 center = (400, 300)
@@ -11,20 +11,22 @@ frame = 0
 while not window.closed:
     # frame start
     start = time()
+    window.clear()  # clear window before redraw
     frame += 1
 
-    angle = ((0.01 * frame) % 2) * pi
+    # redraw frame
+    angle = ((0.01 * frame) % 2) * pi  # radians
     offset = 100 * (1 + cos(2 * angle))
-    point1 = (center[0] + offset * cos(angle), center[1] + offset * sin(2 * angle))
-    point2 = (center[0] - offset * cos(angle), center[1] - offset * sin(2 * angle))
-    window.clear()
+    point1 = (center[0] + offset * cos(angle) - radius, center[1] + offset * sin(2 * angle) - radius)  # (x, y)
+    point2 = (center[0] - offset * cos(angle) - radius, center[1] - offset * sin(2 * angle) - radius)  # (x, y)
+    size = (2 * radius, 2 * radius)  # (width, height)
     if angle < pi:
-        window.draw_ellipse((point1[0] - radius, point1[1] - radius), (2 * radius, 2 * radius), color='green')
-        window.draw_rectangle((point2[0] - radius, point2[1] - radius), (2 * radius, 2 * radius), color='blue')
-    else:
-        window.draw_rectangle((point2[0] - radius, point2[1] - radius), (2 * radius, 2 * radius), color='blue')
-        window.draw_ellipse((point1[0] - radius, point1[1] - radius), (2 * radius, 2 * radius), color='green')
-    window.update()
+        window.draw_ellipse(point1, size, color='green')
+        window.draw_rectangle(point2, size, color='#0000ff')  # https://ru.wikipedia.org/wiki/RGB
+    else:  # reversed order
+        window.draw_rectangle(point2, size, color='#0000ff')  # https://ru.wikipedia.org/wiki/RGB
+        window.draw_ellipse(point1, size, color='green')
+    window.update()  # flush draw commands
 
     # normalize fps
     pause = 1 / fps - (time() - start)
